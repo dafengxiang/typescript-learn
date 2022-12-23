75ace1f5bce7328455bdc5993cb7bbd943ad54ea
dafengxiang <1205679862@qq.com>
2022-12-23 22:05:08 +0800

    feat: 课后作业

--- type-challanfes/00533-easy-concat/template.ts
+++ type-challanfes/00533-easy-concat/template.ts
@@ -1,1 +1,5 @@
-type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U]
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U];
+
+// 课后作业
+type first<T> = T extends [infer F, ...infer L] ? F : never;
+type last<T> =  T extends [...infer F, infer L] ? L : never;
--- type-challanfes/00533-easy-concat/test-case.ts
+++ type-challanfes/00533-easy-concat/test-case.ts
@@ -2,7 +2,7 @@
  * @Description: 合并数组
  * @Author: wangfengxiang
  * @Date: 2022-12-23 21:44:11
- * @LastEditTime: 2022-12-23 21:50:17
+ * @LastEditTime: 2022-12-23 22:04:32
  * @LastEditors: wangfengxiang
  */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -12,4 +12,8 @@ type cases = [
   Expect<Equal<Concat<[], [1]>, [1]>>,
   Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
   Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
+
+// 课后作业
+  Expect<Equal<first<[1, 2]>, 1>>,
+  Expect<Equal<last<[1, 2]>, 2>>,
 ]
